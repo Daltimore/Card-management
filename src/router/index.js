980b2fd5b76ec758f7f1b1ef2c80ae1aeb680306
Daltimore <obinnaleo78@gmail.com>
2021-09-08 13:25:11 +0100

    update travis yml file

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,6 @@ export const router = new VueRouter({
 //Route Guard
 router.beforeEach((to, from, next) => {
   const isAuthenticated = JSON.parse(localStorage.getItem('authObject'));
-  if (to.name !== 'login' && !isAuthenticated) next({ name: 'signup' })
-  else if ( to.name === 'signup') next()
+  if (!isAuthenticated) next({ name: 'login' })
   else next()
 })
